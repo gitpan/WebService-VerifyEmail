84ceeac4bb18a4835c028f2bdb4fc07615782943
Neil Bowers <neil@bowers.com>
2013-09-05 19:08:18 +0000

    Import of NEILB/WebService-VerifyEmail-0.01 from CPAN.
    gitpan-cpan-distribution: WebService-VerifyEmail
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NEILB/WebService-VerifyEmail-0.01.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

---
+++ lib/WebService/VerifyEmail/Response.pm
@@ -0,0 +1,14 @@
+package WebService::VerifyEmail::Response;
+{
+  $WebService::VerifyEmail::Response::VERSION = '0.01';
+}
+
+use Moo;
+
+has authentication_status   => (is => 'ro');
+has limit_status            => (is => 'ro');
+has limit_desc              => (is => 'ro');
+has verify_status           => (is => 'ro');
+has verify_status_desc      => (is => 'ro');
+
+1;
